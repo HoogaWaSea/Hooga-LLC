01db7bae8a48c8bc72e7e8f06e44f05698ff93de
hooga <hoosheng0614@gmail.com>
2022-09-15 11:42:47 -0700

    09152022

---
+++ public/index.js
@@ -0,0 +1,63 @@
+"use strict";
+(function() {
+
+  window.addEventListener("load", init);
+  const BASE_URL = "/products";
+
+  function init() {
+    getProducts();
+    id("search-bar").addEventListener("input", enableSearchBtn);
+    id("search-btn").addEventListener("click", getProducts);
+    id("login-btn").addEventListener("click", login);
+    id("order-btn").addEventListener("click", getOrders);
+    id("cart-btn").addEventListener("click", getCartItems);
+    id("grid-btn").addEventListener("click", switchLayout);
+    id("laptop").addEventListener("click", requestLaptop);
+    id("smartphone").addEventListener("click", requestSmartphone);
+    id("smartwatch").addEventListener("click", requestSmartwatch);
+    id("tv").addEventListener("click", requestTV);
+    id("gameconsole").addEventListener("click", requestGameconsole);
+    id("tablet").addEventListener("click", requestTablet);
+  }
+
+  function requestLaptop() {
+    fetch("/products/laptop")
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(processData)
+      .catch(handleError);
+
+  function requestSmartphone() {
+    fetch("/products")
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(processData)
+      .catch(handleError);
+
+  function requestSmartwatch() {
+    fetch("/products")
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(processData)
+      .catch(handleError);
+
+  function requestTV() {
+    fetch("/products")
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(processData)
+      .catch(handleError);
+
+  function requestGameconsole() {
+    fetch("/products")
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(processData)
+      .catch(handleError);
+
+  function requestTablet() {
+    fetch("/products")
+      .then(statusCheck)
+      .then(resp => resp.json())
+      .then(processData)
+      .catch(handleError);
